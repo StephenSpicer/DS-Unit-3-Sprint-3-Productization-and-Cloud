e577851825231d98b3e0982e152ef526fbba03a7
StephenSpicer <slupsha@gmail.comm>
2021-02-23 11:15:24 -0500

    02

--- twitoff_23/twitoff/app.py
+++ twitoff_23/twitoff/app.py
@@ -1,34 +1,23 @@
-""" Main app/routing file for twitoff """
+"""Main app/routing file for Twitoff"""
 from flask import Flask, render_template
 from .models import DB, User, Tweet, insert_example_users
-
-#FLASK_APP=twitoff flask shell
-
-# create flask application
 def create_app():
-    """ Flask application """
+    """Create Flask Application"""
     app = Flask(__name__)
-
-    app.config["SQLALCHEMY_DATASASE_URI"] = "sqlite:///db.sqlite3"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     DB.init_app(app)
-
-
-
     @app.route("/")
     def root():
-        """ at end point '/'"""
+        """At end point '/'"""
         users = User.query.all()
-               
-        return render_template("base.html", title="Home", user=users)
-    
+        return render_template("base.html", title="Home", users=users)
     @app.route("/reset")
     def reset():
         """reset DB using drop_all()"""
         DB.drop_all()
         DB.create_all()
         insert_example_users()
-        return render_template("base.html", title="RESET", user=User.query.all())
-
+        return render_template("base.html", title="RESET",
+                               users=User.query.all())
     return app
-
